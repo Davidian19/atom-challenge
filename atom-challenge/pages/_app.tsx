ffc345390e2af01f61e6e0358bba3c0894982708
David Ian <78829640+Davidian19@users.noreply.github.com>
2023-08-08 23:52:59 -0300

    Update _app.tsx
    Intermediário para passar informações, pois aqui eu passo todas as props dos dados usados, tanto da localização do usuário, como os dados de pesquisa

--- atom-challenge/pages/_app.tsx
+++ atom-challenge/pages/_app.tsx
@@ -1,6 +1,41 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { Inter } from 'next/font/google'
+import Home from './home'
+import NavBar from './navbar'
+import { useState } from 'react'
+import { useRouter } from 'next/router'
+import NotFound from './notFound'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function App() {
+  const [isSearch, setIsSearch] = useState(false);
+  const [whoNews, setWhoNews] = useState('');
+  const location = useRouter();
+
+  console.log(location.asPath);
+  function handleSearch(value: string) {
+    if (value) {
+      console.log(value);
+      setIsSearch(true);
+      setWhoNews(value);
+    } else{
+      setIsSearch(false);
+    }
+  }
+
+ 
+  return(
+    <div className={`${inter.className}`}>
+      <NavBar currentPage={location.asPath} handleSearch={handleSearch} />
+      {
+        location.asPath !== '/home' && location.asPath !== '/' ? (
+          <NotFound />
+        ) : (
+          <Home isSearch={isSearch} whoNews={whoNews} />
+        )
+      }
+    </div>
+  )
 }
